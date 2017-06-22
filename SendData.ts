d9282814c1aa13d96b389c400b8688ec2a4c95d6
RUDY <RUDY@RUDY-TOSH.hfu-open.fu.hs-furtwangen.de>
2017-06-22 11:46:44 +0200

    .

--- SendData.ts
+++ SendData.ts
@@ -23,8 +23,7 @@ namespace SendData {
 
     function sendRequest(_color: string): void {
         let xhr: XMLHttpRequest = new XMLHttpRequest();
-        xhr.open("GET", "http://localhost:8100?color=" + _color, true);
-        //xhr.open("GET", "https://eia2-servertest.herokuapp.com?color=" + _color, true);
+        xhr.open("GET", "https://servertestruth.herokuapp.com?color=" + _color, true);
         xhr.addEventListener("readystatechange", handleStateChange);
         xhr.send();
     }
